4ec48e034fe15fce18a9fc8313324893f10de8ea
aungmintun1 <92223456+aungmintun1@users.noreply.github.com>
2024-08-22 16:34:26 -0400

    added slides to home page new content

--- components/homeSlider.js
+++ components/homeSlider.js
@@ -20,41 +20,34 @@ const HomeSlider = () => {
     <div className="bannercontainer rev_slider_wrapper">
       <Slider {...settings}>
         <div className="slide">
-            <div className='bg-image-1'> 
+
+        <div className='banner-image-3'> 
+             <div className='slide-info'>
+             <h1 className='slider-text'>Book A Ticket To A Movie</h1>
+             <button className='btn btn-md btn--danger btn--wide slider--btn'>Learn More</button>
+             </div>
+            </div>
+        </div>
+
+        <div className="slide">
+          <div className='bg-image-1'> 
              <div className='slide-info'>
              <h1 className='slider-text'>Raiders Of The Lost Ark</h1>
              <button className='btn btn-md btn--danger btn--wide slider--btn'>Learn More</button>
              </div>
             </div>
-            
-        
-          {/* <img className="rev-slidebg" alt="Rush" src="/images/slides/first-slide.jpg" /> */}
-          
         </div>
 
         <div className="slide">
-          <img className="rev-slidebg" alt="Travel worldwide" src="/images/bg-video.jpg" />
-          <div className="tp-caption slide__name slide__name--smaller">Travel, Admire, Remember.</div>
-          <div className="tp-caption slide__time">From</div>
-          <div className="tp-caption slide__date position-center postion-place--two">April 18</div>
-          <div className="tp-caption slide__time">- till</div>
-          <div className="tp-caption slide__date">May 01</div>
-          <div className="tp-caption slider-wrap-btn">
-            <a href="#" className="btn btn-md btn--danger btn--wide slider--btn">learn more</a>
-          </div>
-        </div>
-
-        <div className="slide">
-          <img className="rev-slidebg" alt="Stop wishing. Start doing." src="/images/slides/next-slide.jpg" />
-          <div className="tp-caption slide__name slide__name--smaller slide__name--specific">
-            Stop <span className="highlight">wishing.</span> Start <span className="highlight">doing.</span>
-          </div>
-          <div className="tp-caption slide__descript">
-            find your best match movie with A.MOVIE
-          </div>
-          <div className="tp-caption slider-wrap-btn">
-            <a href="#" className="btn btn-md btn--danger slider--btn">check out movies</a>
-          </div>
+              <iframe 
+        className="featured-media__video"
+        width="100%"
+        height="100%"
+        src="https://www.youtube.com/embed/6npU3Fw6bb8?autoplay=1&mute=1&loop=1&start=16&controls=0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen 
+        style={{ pointerEvents: 'none' }} // Prevents any interaction with the iframe
+    />
         </div>
       </Slider>
     </div>
